2b98033ddf73fd516a72d4c3d91cbfa5d113fc8b
s27055 <martyna.bawor@gmail.com>
2024-03-12 15:18:12 +0100

    Commit

--- Solution1/ConsoleApp1/Program.cs
+++ Solution1/ConsoleApp1/Program.cs
@@ -2,4 +2,16 @@
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("modyfikacja1");
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
+    return sum / numbers.Length;
+}
 
+int[] arr = { 1, 2, 3, 4 };
+double averge = GetAverage(arr);
+Console.WriteLine(averge);
